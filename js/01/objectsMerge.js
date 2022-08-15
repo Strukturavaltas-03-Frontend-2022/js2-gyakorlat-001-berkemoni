0553cb5d58f67f4a5275d350ab7e03028db3650f
Moni <monika.berke@gmail.com>
2022-08-16 01:05:08 +0200

    1. teljes 2. még nem ment át a teszten

--- js/01/objectsMerge.js
+++ js/01/objectsMerge.js
@@ -1,3 +1,11 @@
-const objectsMerge = () => {};
+'use strict';
+        
+
+const objectsMerge = (...obj) => {
+    const newObject = {...obj}
+    return newObject
+};
+console.log (objectsMerge);
+
 
 export default objectsMerge;
---
+++ js/02/taggedTemplate.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+const taggedTemplate = (texts, ...values) =>
+    texts.map((text, index) =>
+        `${text && text !== ' ' ? <em>${text}</em> : ''} ${values[index] ? `<strong>${values[index]}</strong>` : ''}`
+).join('');
+
+
+export default taggedTemplate;
